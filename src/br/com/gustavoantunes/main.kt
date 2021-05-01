2a3f6bb5949ba697e70f5e3f53b36e75251e8f35
Gustavo Antunes <gas.antunes84@gmail.com>
2021-05-01 16:46:53 -0300

    Utilizando as expressões

--- src/br/com/gustavoantunes/main.kt
+++ src/br/com/gustavoantunes/main.kt
@@ -1,9 +1,19 @@
+import java.lang.NumberFormatException
+
 fun main() {
 
+    val entrada: String = "1,9"
+    val valor: Double? = try {
+       entrada.toDouble()
+    } catch (e: NumberFormatException){
+        print("Problema na conversão")
+        e.printStackTrace()
+        null
+    }
 
-}
-
-fun imprime(message: Any): Any {
-    println(message)
-    return message
+    if(valor != null){
+        print("valor invalido")
+    } else {
+        print("valor $valor")
+    }
 }
---
+++ src/br/com/gustavoantunes/teste/testaAny.kt
@@ -0,0 +1,6 @@
+package br.com.gustavoantunes.teste
+
+fun imprime(message: Any): Any {
+    println(message)
+    return message
+}
